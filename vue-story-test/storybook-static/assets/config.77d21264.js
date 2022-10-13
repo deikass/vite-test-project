59292aafa054f03a9924ebc1048afb506ea0aeb5
Deividas Kasperavičius <deividas.kasperavicius@nfq.lt>
2022-10-13 17:43:44 +0300

    Added Vue3 storybook

---
+++ vue-story-test/storybook-static/assets/config.77d21264.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory.95c34a3e.js";import"./iframe.ebb4f95a.js";import"./vue.esm-bundler.41fea4e1.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config.77d21264.js.map
